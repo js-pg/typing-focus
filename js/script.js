578811f25348bc55336d84e62ded211357f9fc20
BigMuscleMann <83908895+BigMuscleMann@users.noreply.github.com>
2021-09-15 10:41:56 -0500

    reference stack overflow link

--- js/script.js
+++ js/script.js
@@ -8,8 +8,8 @@ mainDiv.style.height = (window.innerHeight)+"px";
 
 function changeTheme(targetTheme){
     document.querySelector("link[href='u1.css']").href = "targetTheme" + ".css";
-}
-
+}// reference: https://stackoverflow.com/questions/19844545/replacing-css-file-on-the-fly-and-apply-the-new-style-to-the-page
+;
 function scrollMain() {
     window.scroll({
         top: 94,
