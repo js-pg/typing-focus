a3383b3fc5d24abd7aa83864eb6286f5f441b145
BigMuscleMann <83908895+BigMuscleMann@users.noreply.github.com>
2021-09-14 21:26:09 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,19 @@
+/*              |
+                |       make main div height of viewport
+               \/
+*/
+const mainDiv = document.getElementById("main");
+mainDiv.style.height = (window.innerHeight)+"px";
+
+function setScrollBar(thumb, track){
+    console.log(thumb, track);
+};
+
+
+
+function scrollMain() {
+    window.scroll({
+        top: 94,
+        behavior: 'smooth'
+    })
+}
